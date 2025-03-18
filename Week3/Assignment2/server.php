d14d1c25f4de09731c5dd8890b223bcc3a9332d9
Ponlork-PK <rousponlork@gmail.com>
2025-03-18 11:33:59 +0700

    Add files via upload

---
+++ Week3/Assignment2/server.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+if (!isset($_SESSION['students'])) {
+    $_SESSION['students'] = [];
+}
+header("Content-Type: application/json");
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+    if ($data["option"] === "add") {
+        $id = count($_SESSION['students']) + 1;
+        $_SESSION['students'][] = ["id" => $id, "name" => $data["name"], "age" => $data["age"], "email" => $data["email"]];
+    } elseif ($data["option"] === "update") {
+        foreach ($_SESSION['students'] as &$student) {
+            if ($student["id"] == $data["id"]) {
+                $student["name"] = $data["name"];
+                $student["age"] = $data["age"];
+                $student["email"] = $data["email"];
+                break;
+            }
+        }
+    } elseif ($data["option"] === "delete") {
+        $_SESSION['students'] = array_values(array_filter($_SESSION['students'], fn($student) => $student["id"] != $data["id"]));
+    }
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode($_SESSION['students']);
+}
+?>
